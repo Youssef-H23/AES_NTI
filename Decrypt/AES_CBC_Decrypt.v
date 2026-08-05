c778652f8e93a4baf6765ef534165a1688807f3a
Radwa Mohamed <taba44968@gmail.com>
2026-08-05 17:12:37 +0300

    Refactor AES_CBC_Decrypt module for CBC mode

--- Decrypt/AES_CBC_Decrypt.v
+++ Decrypt/AES_CBC_Decrypt.v
@@ -1,14 +1,22 @@
 `include "aes_params.vh"
+
 module AES_CBC_Decrypt (
-    input  wire [`STATE_WIDTH-1:0] state_in,
-    input  wire [`STATE_WIDTH-1:0] round_key,
-    output wire [`STATE_WIDTH-1:0] state_out
+
+    input  wire [`STATE_WIDTH-1:0] ciphertext,
+    input  wire [`STATE_WIDTH-1:0] key,
+    input  wire [`STATE_WIDTH-1:0] iv,
+
+    output wire [`STATE_WIDTH-1:0] plaintext
+
 );
 
     Decrypt_Top u_decrypt_top (
-        .state_in  (state_in),
-        .round_key (round_key),
-        .state_out (state_out)
+
+        .ciphertext (ciphertext),
+        .key        (key),
+        .iv         (iv),
+        .plaintext  (plaintext)
+
     );
 
 endmodule
