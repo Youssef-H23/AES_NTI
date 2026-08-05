c7e439af97b6558da919f41335d936b34d39a68c
ebram-adeeb <ebram.adeeb74@gmail.com>
2026-08-05 05:01:34 +0300

    Implement key scheduler module for AES encryption

---
+++ Encrypt/key_scheduler.v
@@ -0,0 +1,25 @@
+`include "aes_params.vh"
+module key_scheduler #(NUM_ROUNDS = 10)(
+    input  wire [4*(NUM_ROUNDS+1)*32-1:0] expanded_key,
+    output wire [`STATE_WIDTH-1:0] round1_key,
+    output wire [`STATE_WIDTH-1:0] round2_key,
+    output wire [`STATE_WIDTH-1:0] round3_key,
+    output wire [`STATE_WIDTH-1:0] round4_key,
+    output wire [`STATE_WIDTH-1:0] round5_key,
+    output wire [`STATE_WIDTH-1:0] round6_key,
+    output wire [`STATE_WIDTH-1:0] round7_key,
+    output wire [`STATE_WIDTH-1:0] round8_key,
+    output wire [`STATE_WIDTH-1:0] round9_key,
+    output wire [`STATE_WIDTH-1:0] round10_key
+);
+    assign round1_key = [1*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round2_key = [2*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round3_key = [3*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round4_key = [4*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round5_key = [5*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round6_key = [6*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round7_key = [7*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round8_key = [8*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round9_key = [9*(`STATE_WIDTH) +:`STATE_WIDTH];
+    assign round10_key = [10*(`STATE_WIDTH) +:`STATE_WIDTH];
+endmodule
