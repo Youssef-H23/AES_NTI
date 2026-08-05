072e11cc24588602ac9f0b65662a943a2d2fa802
Radwa Mohamed <taba44968@gmail.com>
2026-08-05 20:34:58 +0300

    Refactor AES CBC Encrypt testbench for clarity

--- Testbenches/AES_CBC_Encrypt_tb.v
+++ Testbenches/AES_CBC_Encrypt_tb.v
@@ -1,130 +1,195 @@
-`include "aes_params.vh"
 `timescale 1ns/1ps
+
+`include "aes_params.vh"
 
 module AES_CBC_Encrypt_tb;
 
-    reg  [127:0] plaintext;
-    reg  [127:0] key;
-    reg  [127:0] iv;
 
-    wire [127:0] ciphertext;
+    //---------------------------------------------------------
+    // Signals
+    //---------------------------------------------------------
 
-    integer pass_count = 0;
-    integer fail_count = 0;
+    reg  [4*`STATE_WIDTH-1:0] plaintext;
 
-    // Instantiate AES CBC Encrypt Top
+    reg  [`STATE_WIDTH-1:0] key;
+
+    reg  [`STATE_WIDTH-1:0] iv;
+
+
+    wire [4*`STATE_WIDTH-1:0] ciphertext;
+
+
+
+    //---------------------------------------------------------
+    // Expected Ciphertext
+    //---------------------------------------------------------
+
+    reg [4*`STATE_WIDTH-1:0] expected_ciphertext;
+
+
+
+    //---------------------------------------------------------
+    // DUT
+    //---------------------------------------------------------
+
     AES_CBC_Encrypt dut (
-        .plaintext  (plaintext),
-        .key        (key),
-        .iv         (iv),
-        .ciphertext (ciphertext)
+
+        .plaintext (plaintext),
+        .key       (key),
+        .iv        (iv),
+        .ciphertext(ciphertext)
+
     );
+     
+    //---------------------------------------------------------
+    // AES ENCRYPT MONITOR
+    //---------------------------------------------------------
+
+    //---------------------------------------------------------
+    // Test
+    //---------------------------------------------------------
 
     initial begin
+
+
         $display("======================================");
-        $display("  AES CBC ENCRYPT NIST SP 800-38A TEST");
+        $display("      AES CBC ENCRYPT TEST");
         $display("======================================");
+
+
+        //-----------------------------------------------------
+        // NIST AES-128 CBC Test Vector
+        //-----------------------------------------------------
 
         key = 128'h2B7E151628AED2A6ABF7158809CF4F3C;
 
-        //--------------------------------------------------
-        // Block 1 (IV = 000102030405060708090A0B0C0D0E0F)
-        //--------------------------------------------------
-        plaintext = 128'h6BC1BEE22E409F96E93D7E117393172A;
-        iv        = 128'h000102030405060708090A0B0C0D0E0F;
-        #10;
 
-        $display("Block 1 Ciphertext = %h", ciphertext);
-        $display("Expected           = 7649ABAC8119B246CEE98E9B12E9197D");
+        iv = 128'h000102030405060708090A0B0C0D0E0F;
 
-        if (ciphertext === 128'h7649ABAC8119B246CEE98E9B12E9197D) begin
-            pass_count = pass_count + 1;
-            $display("[PASS] CBC Block 1");
-        end else begin
-            fail_count = fail_count + 1;
-            $display("[FAIL] CBC Block 1");
-        end
 
-        //--------------------------------------------------
-        // Block 2 (IV is previous ciphertext Block 1)
-        //--------------------------------------------------
-        plaintext = 128'hAE2D8A571E03AC9C9EB76FAC45AF8E51;
-        iv        = 128'h7649ABAC8119B246CEE98E9B12E9197D;
-        #10;
 
-        $display("Block 2 Ciphertext = %h", ciphertext);
-        $display("Expected           = 5086CB9B507219EE95DB113A917678B2");
+        plaintext = 512'h6BC1BEE22E409F96E93D7E117393172AAE2D8A571E03AC9C9EB76FAC45AF8E5130C81C46A35CE411E5FBC1191A0A52EFF69F2445DF4F9B17AD2B417BE66C3710;
 
-        if (ciphertext === 128'h5086CB9B507219EE95DB113A917678B2) begin
-            pass_count = pass_count + 1;
-            $display("[PASS] CBC Block 2");
-        end else begin
-            fail_count = fail_count + 1;
-            $display("[FAIL] CBC Block 2");
-        end
 
-        //--------------------------------------------------
-        // Block 3 (IV is previous ciphertext Block 2)
-        //--------------------------------------------------
-        plaintext = 128'h30C81C46A35CE411E5FBC1191A0A52EF;
-        iv        = 128'h5086CB9B507219EE95DB113A917678B2;
-        #10;
 
-        $display("Block 3 Ciphertext = %h", ciphertext);
-        $display("Expected           = 73BED6B8E3C1743B7116E69E22229516");
+        expected_ciphertext = 512'h7649ABAC8119B246CEE98E9B12E9197D5086CB9B507219EE95DB113A917678B273BED6B8E3C1743B7116E69E222295163FF1CAA1681FAC09120ECA307586E1A7;
+                                  
+                                  
+                                  
 
-        if (ciphertext === 128'h73BED6B8E3C1743B7116E69E22229516) begin
-            pass_count = pass_count + 1;
-            $display("[PASS] CBC Block 3");
-        end else begin
-            fail_count = fail_count + 1;
-            $display("[FAIL] CBC Block 3");
-        end
 
-        //--------------------------------------------------
-        // Block 4 (IV is previous ciphertext Block 3)
-        //--------------------------------------------------
-        plaintext = 128'hF69F2445DF4F9B17AD2B417BE66C3710;
-        iv        = 128'h73BED6B8E3C1743B7116E69E22229516;
-        #10;
 
-        $display("Block 4 Ciphertext = %h", ciphertext);
-        $display("Expected           = 3FF1CAA1681FAC09120ECA307586E1A7");
+        #20;
 
-        if (ciphertext === 128'h3FF1CAA1681FAC09120ECA307586E1A7) begin
-            pass_count = pass_count + 1;
-            $display("[PASS] CBC Block 4");
-        end else begin
-            fail_count = fail_count + 1;
-            $display("[FAIL] CBC Block 4");
-        end
 
-        //--------------------------------------------------
-        // Additional Test (IV = 0)
-        //--------------------------------------------------
-        plaintext = 128'h3243F6A8885A308D313198A2E0370734;
-        key       = 128'h2B7E151628AED2A6ABF7158809CF4F3C;
-        iv        = 128'h00000000000000000000000000000000;
-        #10;
 
-        $display("Additional Test Ciphertext = %h", ciphertext);
-        $display("Expected                   = 3925841d02dc09fbdc118597196a0b32");
+        $display("====================================================");
+        $display("                  CBC-AES128 (Encryption)");
+        $display("====================================================");
 
-        if (ciphertext === 128'h3925841d02dc09fbdc118597196a0b32) begin
-            pass_count = pass_count + 1;
-            $display("[PASS] Additional Test (IV = 0)");
-        end else begin
-            fail_count = fail_count + 1;
-            $display("[FAIL] Additional Test (IV = 0)");
-        end
 
         $display("");
-        $display("======================================");
-        $display("Simulation Summary:");
-        $display("PASS = %0d, FAIL = %0d", pass_count, fail_count);
+        $display("Key is");
+        $display("    %h", key);
+
+
+
+        $display("");
+        $display("Plaintext is");
+
+        $display("    %h", plaintext[511:384]);
+        $display("    %h", plaintext[383:256]);
+        $display("    %h", plaintext[255:128]);
+        $display("    %h", plaintext[127:0]);
+
+
+
+        //-----------------------------------------------------
+        // CBC Blocks Debug
+        //-----------------------------------------------------
+
+
+        $display("");
+        $display("Block #1");
+
+        $display("    Plaintext     %h", dut.P0);
+        $display("    InputBlock    %h", dut.X0);
+        $display("    Ciphertext    %h", dut.C0);
+
+
+
+        $display("");
+        $display("Block #2");
+
+        $display("    Plaintext     %h", dut.P1);
+        $display("    InputBlock    %h", dut.X1);
+        $display("    Ciphertext    %h", dut.C1);
+
+
+
+        $display("");
+        $display("Block #3");
+
+        $display("    Plaintext     %h", dut.P2);
+        $display("    InputBlock    %h", dut.X2);
+        $display("    Ciphertext    %h", dut.C2);
+
+
+
+        $display("");
+        $display("Block #4");
+
+        $display("    Plaintext     %h", dut.P3);
+        $display("    InputBlock    %h", dut.X3);
+        $display("    Ciphertext    %h", dut.C3);
+
+
+
+        $display("");
+        $display("Ciphertext is");
+
+        $display("    %h", ciphertext[511:384]);
+        $display("    %h", ciphertext[383:256]);
+        $display("    %h", ciphertext[255:128]);
+        $display("    %h", ciphertext[127:0]);
+
+
+
+        $display("");
+        $display("****************************************************");
+        $display("====================================================");
+
+
+
+        //-----------------------------------------------------
+        // Check Result
+        //-----------------------------------------------------
+
+        if (ciphertext == expected_ciphertext) begin
+
+            $display("[PASS] AES CBC Encryption");
+
+        end
+
+        else begin
+
+            $display("[FAIL] AES CBC Encryption");
+
+            $display("Expected = %h", expected_ciphertext);
+
+            $display("Actual   = %h", ciphertext);
+
+        end
+
+
+
         $display("======================================");
 
-        $stop;
+
+        $finish;
+
+
     end
+
 
 endmodule
